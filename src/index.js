3e15a12955f19c00a300de9e5fada5f88ba0844c
Juan Diego <jcarranzav1@hotmail.com>
2021-11-29 23:46:36 -0500

    File sorting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/styles.scss';
 
 function render() {
-  ReactDOM.render(<App />, document.getElementById('root'));
+	ReactDOM.render(<App />, document.getElementById('root'));
 }
 
 render();
