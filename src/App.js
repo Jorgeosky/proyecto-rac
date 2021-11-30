1840e3cdc9981516d50cf521237f2c7a7f1cf201
Juan Diego <jcarranzav1@hotmail.com>
2021-11-29 23:46:36 -0500

    File sorting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import { Login } from './pages/Login';
-import ForgetPass from './pages/ForgetPass';
-import SignUp from './pages/SignUp';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import { AppRouter } from './routes/AppRouter';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/ForgetPass" element={<ForgetPass />} />
-        <Route path="/SignUp" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return <AppRouter />;
 }
 
 export default App;
