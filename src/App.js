7690eecacaa82f839e6dc5d0c9c9a09768b754b1
Jorge Quintero <jodaquin02@gmail.com>
2021-12-02 16:01:17 -0500

    New Design structure items and fixs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import { AppRouter } from './routes/AppRouter';
-=======
-import { Login } from './pages/Login';
-import ForgetPass from './pages/ForgetPass';
-import SignUp from './pages/SignUp';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import './styles/App.css';
->>>>>>> Design Script using UseState
 
 function App() {
   return <AppRouter />;
