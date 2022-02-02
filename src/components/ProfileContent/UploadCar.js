7d0c56633d82aa7f626f4d182925c464284b118d
Juan Vargas <juandavc12@hotmail.com>
2022-02-02 16:54:44 -0500

    add uploadCar to profile

--- src/components/ProfileContent/UploadCar.js
+++ src/components/ProfileContent/UploadCar.js
@@ -1,16 +1,20 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function UploadCar() {
   return (
-    <div>
+    <div className="carForm">
+      <h1>Upload car</h1>
       <form>
-        <p>modelo</p>
-        <input type="text" />
-        <p>precio</p>
-        <input type="number" />
-        <p>foto</p>
-        <input alt="carimage" type="file" />
-        <button type="submit">Enviar</button>
+        <div className="uploadCar">
+          <input className="field" name="modelCar" placeholder="Model" type="text" />
+          <input className="fieldPrice" name="priceCar" placeholder="Price" type="number" />
+          <Button component="label" variant="contained">
+            Upload image
+            <input className="as" type="file" hidden />
+          </Button>
+          <button type="submit">Enviar</button>
+        </div>
       </form>
     </div>
   );
