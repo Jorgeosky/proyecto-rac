6b374bdf05f0b06a7ae94287143c1188dbae0add
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-04 14:41:22 -0500

    Fix merge conflict cardpage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { NavbarSearch } from './components/searchVehicle/NavbarSearch';
+import CarPage from './pages/CarPage';
 import ChangePassword from './pages/ChangePassword';
 import ConfirmEmail from './pages/ConfirmEmail';
 import { EmailVerified } from './pages/EmailVerified';
@@ -23,6 +24,7 @@ function App() {
         <Route element={<ChangePassword />} path="/changepass" />
         <Route element={<SignUp />} path="/signup" />
         <Route element={<SearchPage />} path="/search" />
+        <Route element={<CarPage />} path="/carpage" />
         <Route element={<ConfirmEmail />} path="/confirmemail" />
         <Route path="/emailverified">
           <Route element={<EmailVerified />} path=":token" />
