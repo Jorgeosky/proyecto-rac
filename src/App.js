353ab1819e4940aa50720055f091fbde88445463
Juan Diego Carranza Vega <juan.carranza@smartpyme.pe>
2022-02-03 20:58:14 -0500

    Delete App AppRouter Refactor BrowseRouter and UseContext in the index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { AppRouter } from './routes/AppRouter';
+import { Route, Routes } from 'react-router-dom';
+import { NavbarSearch } from './components/searchVehicle/NavbarSearch';
+import ChangePassword from './pages/ChangePassword';
+import ConfirmEmail from './pages/ConfirmEmail';
+import { EmailVerified } from './pages/EmailVerified';
+import ForgetPass from './pages/ForgetPass';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import SearchPage from './pages/SearchPage';
+import SignUp from './pages/SignUp';
 
 function App() {
-  return <AppRouter />;
+  return (
+    <>
+      <NavbarSearch />
+      <Routes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Login />} path="/signin" />
+        <Route element={<Profile />} path="/profile" />
+        <Route element={<ForgetPass />} path="/forgetpass" />
+        <Route element={<ChangePassword />} path="/changepass" />
+        <Route element={<SignUp />} path="/signup" />
+        <Route element={<SearchPage />} path="/search" />
+        <Route element={<ConfirmEmail />} path="/confirmemail" />
+        <Route path="/emailverified">
+          <Route element={<EmailVerified />} path=":token" />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { UserProvider } from './components/Context';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 
 function render() {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  ReactDOM.render(
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>,
+    document.getElementById('root'),
+  );
 }
 
 render();
