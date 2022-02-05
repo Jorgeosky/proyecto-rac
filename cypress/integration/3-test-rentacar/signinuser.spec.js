2d3a691d722ee8e12bba67c79e79c6482c3a3a63
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-05 14:47:33 -0500

    add cypress test

---
+++ cypress/integration/3-test-rentacar/signinuser.spec.js
@@ -0,0 +1,14 @@
+describe("RentaCar App", () => {
+  it("sign-in existent user(Renter) and redirect to Renter Profile ", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".dropdown-toggle").click();
+    cy.get('a[href*="signin"]').click();
+    cy.get(".TypeUser").click('right')
+    
+    cy.get('form').within(() => {
+      cy.get('#email').type('juan.carlos.moreno.banda@gmail.com') 
+      cy.get('#password').type('Renta@car2022')
+      cy.get('.submit').click()
+  }) 
+  });
+});
---
+++ cypress/integration/3-test-rentacar/signup.spec.js
@@ -0,0 +1,18 @@
+describe("RentaCar App", () => {
+  it("sign-up as Renter", () => {
+    cy.visit("http://localhost:3000");
+    cy.get(".dropdown-toggle").click();
+    cy.get('a[href*="signup"]').click();
+    cy.get(".TypeUser").click('right')
+    
+    cy.get('form').within(($form) => {
+      cy.get('input[name="firstName"]').type('Juan Carlos')
+      cy.get('input[name="lastName"]').type('Moreno Banda')
+      cy.get('input[name="email"]').type('juan.moreno.banda@gmail.com')
+      cy.get('input[name="password"]').type('Renta@Car2022')
+      cy.get('input[name="confirmPassword"]').type('Renta@Car2022')
+      cy.get('.confirm').click() 
+      cy.root().submit()    
+    })
+  });
+});
