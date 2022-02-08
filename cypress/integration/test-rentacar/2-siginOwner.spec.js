236b2b98a884b43d1bf9f28a9677cca172ad09a1
Juan C. Moreno B <juan.carlos.moreno.banda@hotmail.com>
2022-02-08 14:32:23 -0500

    signInOwner and upload a car with cypress

--- cypress/integration/test-rentacar/2-siginOwner.spec.js
+++ cypress/integration/test-rentacar/2-siginOwner.spec.js
@@ -17,7 +17,16 @@ describe("RentaCar App", () => {
     cy.get('form').within(() => { 
       cy.get('input[name="model"]').type('Toyota Prius 2020')
       cy.get('input[name="price"]').type('80')
-      cy.get('input[type="file"]').attachFile('ToyotaPrius2020front.jpg', { subjectType: 'input' })      
+      cy.get('#photo1').attachFile('ToyotaPrius2020front.jpg', { subjectType: 'input' })
+      cy.get('#photo2').attachFile('ToyotaPrius2020photo1.jpg', { subjectType: 'input' })
+      cy.get('#photo3').attachFile('ToyotaPrius2020photo2.jpg', { subjectType: 'input' })
+      cy.get('#photo4').attachFile('ToyotaPrius2020photo3.jpg', { subjectType: 'input' }) 
+      cy.get('#react-select-2-input').type('Toyota').type('{enter}') 
+      cy.get('#react-select-3-input').type('4').type('{enter}') 
+      cy.get('#react-select-4-input').type('4 or more').type('{enter}') 
+      cy.get('#react-select-5-input').type('Cars').type('{enter}') 
+      cy.get('textarea[name="description"]').type('The New Toyota Prius 2020 just for your trip')
+      cy.root().submit()
     })
   });
 });
